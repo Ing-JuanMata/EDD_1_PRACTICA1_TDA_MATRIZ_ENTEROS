94518f0d13e4003d73e1e7af9b71c7ac6485497d
JUAN JESUS MATA SOLIS <jujemataso@ittepic.edu.mx>
2021-03-11 15:27:33 -0700

    Base del proyecto

---
+++ src/Matriz.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ING-JUANMATA
+ */
+public class Matriz {
+
+    private int tam;
+    private int[][] datos;
+
+    public Matriz(int tam) {
+        this.tam = tam;
+        datos = new int[tam][tam];
+    }
+
+}
